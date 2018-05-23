6db5086f6396c0235737a0b70b61ed8b4f0e7e7e
gismo <gismo8@gmail.com>
2018-05-24 00:10:18 +0200

    ExoPlayer and navigationContainer added

--- app/src/main/java/com/example/gismo/chefsteps/RecipeDetailFragment.java
+++ app/src/main/java/com/example/gismo/chefsteps/RecipeDetailFragment.java
@@ -1,6 +1,8 @@
 package com.example.gismo.chefsteps;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +18,21 @@ import com.example.gismo.chefsteps.network.model.IngredientWrapper;
 import com.example.gismo.chefsteps.network.model.Recipe;
 import com.example.gismo.chefsteps.network.model.RecipeDetail;
 import com.example.gismo.chefsteps.network.model.Step;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,7 +46,11 @@ public class RecipeDetailFragment extends ChefStepFragment {
     @BindView(R.id.title)
     TextView title;
 
+    @BindView(R.id.playerView)
+    SimpleExoPlayerView playerView;
+
     protected RecipeDetail recipeDetail;
+    protected SimpleExoPlayer player;
 
     public RecipeDetailFragment() {
     }
@@ -39,6 +60,14 @@ public class RecipeDetailFragment extends ChefStepFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recipe_details, container, false);
         ButterKnife.bind(this, view);
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        player = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
+        playerView.setPlayer(player);
         bind(recipeDetail);
         return view;
     }
@@ -50,10 +79,23 @@ public class RecipeDetailFragment extends ChefStepFragment {
         }
         if (recipeDetail instanceof Step) {
             Step step = (Step) recipeDetail;
-            title.setText(step.getShortDescription());
+            title.setText(step.getDescription());
+            if (player != null) {
+                preparePlayer(step.getVideoURL());
+                player.setPlayWhenReady(true);
+            }
         } else {
             IngredientWrapper ingredientWrapper = (IngredientWrapper) recipeDetail;
             title.setText(ingredientWrapper.getIngredients().get(0).getIngredient());
         }
+    }
+
+    public void preparePlayer(String url) {
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
+                Util.getUserAgent(getContext(), "ChefSteps"), bandwidthMeter);
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(url));
+        player.prepare(videoSource);
     }
 }
--- app/src/main/java/com/example/gismo/chefsteps/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/gismo/chefsteps/RecipeDetailsActivity.java
@@ -1,18 +1,32 @@
 package com.example.gismo.chefsteps;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.gismo.chefsteps.adapter.RecipeAdapter;
 import com.example.gismo.chefsteps.core.ChefStepActivity;
 import com.example.gismo.chefsteps.network.model.Recipe;
 import com.example.gismo.chefsteps.network.model.RecipeDetail;
 import com.example.gismo.chefsteps.ui.view.DetailView;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +40,9 @@ public class RecipeDetailsActivity extends ChefStepActivity implements DetailVie
 
     @BindView(R.id.fragmentContainer)
     FrameLayout fragmentContainer;
+
+    @BindView(R.id.navigationContainer)
+    LinearLayout navigationContainer;
 
     protected Recipe recipe;
     protected RecipeDetailsListFragment detailListFragment;
@@ -48,8 +65,6 @@ public class RecipeDetailsActivity extends ChefStepActivity implements DetailVie
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(recipe.getName());
-
-        Timber.log(1, recipe.toString());
     }
 
     @Override
@@ -66,14 +81,34 @@ public class RecipeDetailsActivity extends ChefStepActivity implements DetailVie
     public void onClick(RecipeDetail recipeDetail) {
         detailFragment = new RecipeDetailFragment();
         detailFragment.bind(recipeDetail);
+        Animation slideInAnimation = AnimationUtils.loadAnimation(getApplication(), R.anim.slide_in_from_bottom);
+        navigationContainer.setAnimation(slideInAnimation);
+        slideInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                navigationContainer.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        slideInAnimation.start();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, detailFragment).commit();
     }
 
     @Override
     public void onBackPressed() {
         if (detailFragment != null) {
+            navigationContainer.setVisibility(View.GONE);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, detailListFragment).commit();
             detailFragment = null;
+
         } else {
             super.onBackPressed();
         }
